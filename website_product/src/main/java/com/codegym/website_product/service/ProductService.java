d62d6b8731f5b2cd3d944090d645c3ea37348e68
hoangNH <hoangcdnbk@gmail.com>
2025-01-06 19:44:03 +0700

    update code

---
+++ website_product/src/main/java/com/codegym/website_product/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.codegym.website_product.service;
+
+import com.codegym.website_product.entity.Product;
+import com.codegym.website_product.repository.ProductRepository;
+
+import java.util.List;
+
+public class ProductService {
+    private static ProductRepository productRepository = new ProductRepository();
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public List<Product> getAllByCategory(int categoryId) {
+        return productRepository.getAllByCategory(categoryId);
+    }
+}
